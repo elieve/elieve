370cb51c7b3a956f5be557639b218f956e41cdce
elieve <bayustiadi@ymail.com>
2022-05-30 03:20:29 +0700

    Update abelia.py

--- abelia.py
+++ abelia.py
@@ -2,11 +2,11 @@ import asyncio
 from time import sleep
 
 @ultroid_cmd(pattern="abel")
-async def bye(ult):
+async def bye(ult)(event):
  await ult.edit("ABELIA JELEK! GAPUNYA PACAR YGY")
  sleep(1)
 
-await ult.edit_or_reply("""
+await ult.edit("""
  
          ┏━━┳┓╋╋┏┳━━━┓
          ┃┏┓┃┗┓┏┛┃┏━━┛
