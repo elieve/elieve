d0404252ba17bce4646348b4cfd4de2b38608524
elieve <bayustiadi@ymail.com>
2022-06-01 13:04:05 +0700

    Create asupan.py

---
+++ asupan.py
@@ -0,0 +1,71 @@
+
+
+from secrets import choice
+
+from telethon.tl.types import InputMessagesFilterVideo, InputMessagesFilterVoice
+
+from userbot.utils import edit_or_reply, Ultroid_cmd
+
+
+@Ultroid_cmd(pattern="asupan$")
+async def _(event):
+    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@tedeasupancache", filter=InputMessagesFilterVideo
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan video asupan.**")
+
+
+@Ultroid_cmd(pattern="desahcewe$")
+async def _(event):
+    if event.chat_id in BLACKLIST_CHAT:
+        return await edit_or_reply(
+            event, "**Perintah ini Dilarang digunakan di Group ini**"
+        )
+    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+    try:
+        desahcewe = [
+            desah
+            async for desah in event.client.iter_messages(
+                "@desahancewesangesange", filter=InputMessagesFilterVoice
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(desahcewe), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan desahan cewe.**")
+
+
+@Ultroid_cmd(pattern="desahcowo$")
+async def _(event):
+    if event.chat_id in BLACKLIST_CHAT:
+        return await edit_or_reply(
+            event, "**Perintah ini Dilarang digunakan di Group ini**"
+        )
+    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+    try:
+        desahcowo = [
+            desah
+            async for desah in event.client.iter_messages(
+                "@desahancowokkkk", filter=InputMessagesFilterVoice
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(desahcowo), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan desahan cowo.**")
+
+
