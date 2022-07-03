05dcc2bfda427aee217a1b177a56d18df6692bb3
elieve <bayustiadi@ymail.com>
2022-07-03 20:06:02 +0800

    Update abelia.py

--- abelia.py
+++ abelia.py
@@ -3,13 +3,13 @@ from time import sleep
 
 @ultroid_cmd(pattern="ibel")
 async def ibel(xx):
- xx = await xx.edit("ABELIA JELEK! GAPUNYA PACAR YGY")
+ await xx.edit("ABELIA JELEK! GAPUNYA PACAR YGY")
  await asyncio.sleep(3)
  await xx.edit("Enur yang paling cantik ygy")
  await asyncio.sleep(1)
  await xx.edit("Run! Run! Runn!!!")
  await asyncio.sleep(1)
- xx = await xx.edit("""
+ await xx.edit("""
  
 ┏━━┳┓╋╋┏┳━━━┓
 ┃┏┓┃┗┓┏┛┃┏━━┛
