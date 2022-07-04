9d8f9167d4a3116804353e1ba48fadc1f6f04be3
elieve <bayustiadi@ymail.com>
2022-07-04 18:18:30 +0800

    Update abelia.py

--- abelia.py
+++ abelia.py
@@ -2,13 +2,13 @@ import asyncio
 from time import sleep
 
 @ultroid_cmd(pattern="me")
-async def _(event):
- me = await event.client.get_me()
- await edit_or_reply("HAI JELEK!")
+async def _(xx):
+ me = await xx.client.get_me()
+ xx = await xx.edit("HAI JELEK!")
  await asyncio.sleep(3)
- await xx.edit(event, f"**KENALIN NIH {me.first_name}**")
+ await xx.edit(xx, f"**KENALIN NIH {me.first_name}**")
  await asyncio.sleep(1)
- await xx.edit(event, f"**{me.first_name} ORANG PALING CAKEP SE TELEGRAM**")
+ await xx.edit(xx, f"**{me.first_name} ORANG PALING CAKEP SE TELEGRAM**")
  await asyncio.sleep(1)
  await xx.edit("GAK KEK ELU")
  await asyncio.sleep(1)
