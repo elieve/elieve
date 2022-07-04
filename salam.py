0f3e682a19cc0e7b2a77a6ae8bf3ad68b0e19f09
elieve <bayustiadi@ymail.com>
2022-07-04 12:55:31 +0800

    Update salam.py

--- salam.py
+++ salam.py
@@ -14,23 +14,26 @@ async def _(event):
 
 @ultroid_cmd(pattern="o(?: |$)(.*)")
 async def _(event):
+    await asyncio.sleep(2)
     await event.client.send_message(
       "**Assalamualaikum Warahmatullahi Wabarakatuh**",
-        reply_to=event.reply_to_msg_id,
     )
+    
     await event.delete()
 
 
 @ultroid_cmd(pattern="P(?: |$)(.*)")
 async def _(event):
+    await asyncio.sleep(2)
     me = await event.client.get_me()
     xx = await edit_or_reply(event, f"**Haii Salken Saya {me.first_name}**")
-    asyncio.sleep(2)
+    await asyncio.sleep(2)
     await xx.edit("**Assalamualaikum...**")
 
 
 @ultroid_cmd(pattern="l(?: |$)(.*)")
 async def _(event):
+    await asyncio.sleep(2)
     await event.client.send_message(
         event.chat_id, "**Wa'alaikumsalam**", reply_to=event.reply_to_msg_id
     )
@@ -39,31 +42,36 @@ async def _(event):
 
 @ultroid_cmd(pattern="i(?: |$)(.*)")
 async def _(event):
+    await asyncio.sleep(2)
     me = await event.client.get_me()
     xx = await edit_or_reply(event, f"**Haii Salken Saya {me.first_name}**")
-    asyncio.sleep(2)
+    await asyncio.sleep(2)
     await xx.edit("**Assalamualaikum**")
 
 
 @ultroid_cmd(pattern="j(?: |$)(.*)")
 async def _(event):
+    await asyncio.sleep(2)
     xx = await edit_or_reply(event, "**JAKA SEMBUNG BAWA GOLOK**")
-    asyncio.sleep(3)
+    await asyncio.sleep(3)
     await xx.edit("**NIMBRUNG GOBLOKK!!!🔥**")
 
 
 @ultroid_cmd(pattern="k(?: |$)(.*)")
 async def _(event):
+    await asyncio.sleep(2)
     me = await event.client.get_me()
     xx = await edit_or_reply(event, f"**Hallo KIMAAKK SAYA {me.first_name}**")
-    asyncio.sleep(2)
+    await asyncio.sleep(2)
     await xx.edit("**LU SEMUA NGENTOT 🔥**")
 
 
 @ultroid_cmd(pattern="s(?: |$)(.*)")
 async def _(event):
+    
+    await asyncio.sleep(2)
     xx = await edit_or_reply(event, "**Salam Dulu Biar Sopan**")
-    asyncio.sleep(2)
+    await asyncio.sleep(2)
     await xx.edit("**السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ**")
 
 
