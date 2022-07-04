19431b0ca607e89cad3f713e510b0acfffd2bac8
elieve <bayustiadi@ymail.com>
2022-07-04 18:39:29 +0800

    Update knyl.py

--- knyl.py
+++ knyl.py
@@ -5,17 +5,17 @@ from time import sleep
 async def _(xx):
  await xx.edit("HAI JELEK!")
  await asyncio.sleep(3)
- await xxx.edit(xx, f"**KENALIN NIH**")
+ await xx.edit(xx, f"**KENALIN NIH**")
  await asyncio.sleep(1)
- await xxx.edit(xx, f"**ORANG PALING CAKEP SE TELEGRAM**")
+ await xx.edit(xx, f"**ORANG PALING CAKEP SE TELEGRAM**")
  await asyncio.sleep(1)
- await xxx.edit("GAK KEK ELU")
+ await xx.edit("GAK KEK ELU")
  await asyncio.sleep(1)
- await xxx.edit("HEHEHE")
+ await xx.edit("HEHEHE")
  await asyncio.sleep(1)
- await xxx.edit("Run! Run! Runn!!!")
+ await xx.edit("Run! Run! Runn!!!")
  await asyncio.sleep(1)
- await xxx.edit("""
+ await xx.edit("""
  
 ┏━━┳┓╋╋┏┳━━━┓
 ┃┏┓┃┗┓┏┛┃┏━━┛
