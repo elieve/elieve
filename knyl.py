124fbc0227930ce0a1c1de362f905a3a589aba61
elieve <bayustiadi@ymail.com>
2022-07-04 18:37:50 +0800

    Update and rename abelia.py to knyl.py

--- knyl.py
+++ knyl.py
@@ -3,12 +3,11 @@ from time import sleep
 
 @ultroid_cmd(pattern="me")
 async def _(xx):
- me = await xx.client.get_me()
- xxx = await xx.client.send_message("HAI JELEK!")
+ await xx.edit("HAI JELEK!")
  await asyncio.sleep(3)
- await xxx.edit(xx, f"**KENALIN NIH {me.mention}**")
+ await xxx.edit(xx, f"**KENALIN NIH**")
  await asyncio.sleep(1)
- await xxx.edit(xx, f"**{me.mention} ORANG PALING CAKEP SE TELEGRAM**")
+ await xxx.edit(xx, f"**ORANG PALING CAKEP SE TELEGRAM**")
  await asyncio.sleep(1)
  await xxx.edit("GAK KEK ELU")
  await asyncio.sleep(1)
